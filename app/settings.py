bf6adc8953b54447520d38b96881bd755e675cbf
Akram Rakhmetulla <akram042006@gmail.com>
2025-03-17 00:41:33 +0900

    media fixed

--- app/settings.py
+++ app/settings.py
@@ -144,8 +144,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ('static',)
 STATIC_ROOT = BASE_DIR / "staticfiles" 
+
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
