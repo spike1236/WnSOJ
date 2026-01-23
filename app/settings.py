6dde1e57a2a90d337a50bbee1bcc7960a37432a8
Akram Rakhmetulla <akram042006@gmail.com>
2026-01-22 22:45:25 -0800

    changed DB connection, frontend rework

--- app/settings.py
+++ app/settings.py
@@ -150,10 +150,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "wnsoj_service", # ~/.pg_service.conf
-            "passfile": ".pgpass",
-        },
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
